e002bac79c1e10cd9dbe5592ab05e2533515e3e0
weekly-geekly <donaldfur8@gmail.com>
2019-07-27 15:58:46 +0300

    Add articles

--- js/main.js
+++ js/main.js
@@ -1,4 +1,10 @@
 $(function() {
+	const $bodyElement = $('body');
+
+  const getRandomElement = function(array) { 
+	  return array[Math.floor(Math.random() * (array.length - 0) + 0)]; 
+	};
+
 	//Operate with Page Headings menu
 	const pageHeadingsButton = document.querySelector('.js-list-of-headings-button');
 	const pageHeadingsContainer = document.querySelector('.js-page-headings-container');
@@ -84,66 +90,62 @@ $(function() {
 	//Donate Button
 	const $donateButton = $('.donate-btn');
 
-	const getRandomElement = function(array) { 
-	  return array[Math.floor(Math.random() * (array.length - 0) + 0)]; 
-	};
+	// const arrayOfDonateButtons = [
+	// 	{
+	// 		'bgColor': 'orange',
+	// 		'textColor': 'blue',
+	// 		'text': 'DONATE',
+	// 		'link': 'http://bit.ly/2TTe9my'
+	// 	},
+	// 	{
+	// 		'bgColor': 'red',
+	// 		'textColor': 'black',
+	// 		'text': 'DONATE',
+	// 		'link': 'http://bit.ly/2Y4FYYh'
+	// 	},
+	// 	{
+	// 		'bgColor': 'green',
+	// 		'textColor': 'blue',
+	// 		'text': 'DONATE',
+	// 		'link': 'http://bit.ly/2HofpIN'
+	// 	},
+	// 	{
+	// 		'bgColor': 'aqua',
+	// 		'textColor': 'blue',
+	// 		'text': 'DONATE',
+	// 		'link': 'http://bit.ly/2HDrysq'
+	// 	},
+	// 	{
+	// 		'bgColor': '#3a3a3a'/*grey*/,
+	// 		'textColor': 'blue',
+	// 		'text': 'DONATE',
+	// 		'link': 'http://bit.ly/2FhVl8q'
+	// 	},
+	// 	{
+	// 		'bgColor': '#3a3a3a'/*grey*/,
+	// 		'textColor': 'white',
+	// 		'text': 'DONATE',
+	// 		'link': 'http://bit.ly/2FhVl8q'
+	// 	},
+	// 	{
+	// 		'bgColor': '#2196f3'/*lightblue*/,
+	// 		'textColor': 'white',
+	// 		'text': 'DONATE',
+	// 		'link': 'http://bit.ly/2UGBNjs'
+	// 	}
+	// ];
 
-	const arrayOfDonateButtons = [
-		{
-			'bgColor': 'orange',
-			'textColor': 'blue',
-			'text': 'DONATE',
-			'link': 'http://bit.ly/2TTe9my'
-		},
-		{
-			'bgColor': 'red',
-			'textColor': 'black',
-			'text': 'DONATE',
-			'link': 'http://bit.ly/2Y4FYYh'
-		},
-		{
-			'bgColor': 'green',
-			'textColor': 'blue',
-			'text': 'DONATE',
-			'link': 'http://bit.ly/2HofpIN'
-		},
-		{
-			'bgColor': 'aqua',
-			'textColor': 'blue',
-			'text': 'DONATE',
-			'link': 'http://bit.ly/2HDrysq'
-		},
-		{
-			'bgColor': '#3a3a3a'/*grey*/,
-			'textColor': 'blue',
-			'text': 'DONATE',
-			'link': 'http://bit.ly/2FhVl8q'
-		},
-		{
-			'bgColor': '#3a3a3a'/*grey*/,
-			'textColor': 'white',
-			'text': 'DONATE',
-			'link': 'http://bit.ly/2FhVl8q'
-		},
-		{
-			'bgColor': '#2196f3'/*lightblue*/,
-			'textColor': 'white',
-			'text': 'DONATE',
-			'link': 'http://bit.ly/2UGBNjs'
-		}
-	];
+	// const donateButtonText = 'Plant a 🌴 for 1 💲';
+	// const donateButtonBgColor = '#2196f3';
+	// const donateButtonTextColor = 'white';
 
-	const donateButtonText = 'Plant a 🌴 for 1 💲';
-	const donateButtonBgColor = '#2196f3';
-	const donateButtonTextColor = 'white';
-
-	$donateButton.html(donateButtonText);
-	$donateButton.attr('href', 'http://bit.ly/32oCLF9');
-	$donateButton.css({
-		'background-color': donateButtonBgColor,
-		'color': donateButtonTextColor,
-		'top': $donateButton.width() + $('.js-page-header').height()
-	});
+	// $donateButton.html(donateButtonText);
+	// $donateButton.attr('href', 'http://bit.ly/32oCLF9');
+	// $donateButton.css({
+	// 	'background-color': donateButtonBgColor,
+	// 	'color': donateButtonTextColor,
+	// 	'top': $donateButton.width() + $('.js-page-header').height()
+	// });
 
 
 	//2048 Game
@@ -158,8 +160,69 @@ $(function() {
 
 
   //Add Ads notification
-  // $('<div class="ad-click-notification js-ad-click-notification">Please click on Ads to support the site</div>').appendTo('body');
-  // window.setTimeout(function() {
-  //   $('.js-ad-click-notification').slideDown(2000);
-  // }, 10 * 1000);
+  $('<div class="ad-click-notification js-ad-click-notification">Please click on Ads to support the site</div>').appendTo($bodyElement);
+  window.setTimeout(function() {
+    $('.js-ad-click-notification').slideDown(2000);
+  }, 10 * 1000);
+
+
+  //Amazon Ads Side Widget
+  const arrayOfAmazonIframeSrcs = [
+    'https://ws-na.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=US&source=ac&ref=tf_til&ad_type=product_link&tracking_id=geeksworld0d-20&marketplace=amazon&region=US&placement=B0029XHGRQ&asins=B0029XHGRQ&linkId=3da0981db657d0cc91c3cc600f358778&show_border=false&link_opens_in_new_window=false&price_color=333333&title_color=000dbf&bg_color=ffffff',
+    'https://ws-na.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=US&source=ac&ref=tf_til&ad_type=product_link&tracking_id=geeksworld0d-20&marketplace=amazon&region=US&placement=032157351X&asins=032157351X&linkId=86582b0f2183cd724ba7a44ada1ed13a&show_border=false&link_opens_in_new_window=false&price_color=333333&title_color=000dbf&bg_color=ffffff',
+    'https://ws-na.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=US&source=ac&ref=tf_til&ad_type=product_link&tracking_id=geeksworld0d-20&marketplace=amazon&region=US&placement=0671530771&asins=0671530771&linkId=daf06e768d9f45248059d5e56f934521&show_border=false&link_opens_in_new_window=false&price_color=333333&title_color=000dbf&bg_color=ffffff'
+  ];
+
+  const amazonSideWidget = {
+  	$self: $('<div class="amazon-side-widget is-closed" />'),
+  	$title: $('<div class="amazon-side-widget-title">Books for geeks:</div>'),
+  	$iframe: $('<iframe style="width:120px;height:240px;" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="" />'),
+  	$prevBtn: $('<div class="amazon-side-widget-prev-btn amazon-side-widget-nav-btn"><i class="fa fa-chevron-circle-left"></i></div>'),
+  	$nextBtn: $('<div class="amazon-side-widget-next-btn amazon-side-widget-nav-btn"><i class="fa fa-chevron-circle-right"></i></div>'),
+  	$moreBtn: $('<a class="amazon-side-widget-more-btn" href="https://www.amazon.com/gp/search?ie=UTF8&tag=geeksworld0d-20&linkCode=ur2&linkId=44eac47bbe0d7de17a768bb6a00fcdf2&camp=1789&creative=9325&index=books&keywords=geeks">Read more</a><img src="https://ir-na.amazon-adsystem.com/e/ir?t=geeksworld0d-20&l=ur2&o=1" width="1" height="1" border="0" alt="" style="border:none !important; margin:0px !important;" />'),
+  	$closeBtn: $('<div class="amazon-side-widget-close-btn"><i class="fa fa-angle-double-right"></i></div>')
+  }
+
+  amazonSideWidget.$title.appendTo(amazonSideWidget.$self);
+  amazonSideWidget.$iframe.appendTo(amazonSideWidget.$self);
+  amazonSideWidget.$iframe.attr('src', getRandomElement(arrayOfAmazonIframeSrcs));
+  amazonSideWidget.$prevBtn.appendTo(amazonSideWidget.$self);
+  amazonSideWidget.$nextBtn.appendTo(amazonSideWidget.$self);
+  amazonSideWidget.$moreBtn.appendTo(amazonSideWidget.$self);
+  amazonSideWidget.$closeBtn.appendTo(amazonSideWidget.$self);
+
+  amazonSideWidget.$self.appendTo($bodyElement);
+
+  const getAmznIframeSrc = function(direction) {
+    const currentIndex = arrayOfAmazonIframeSrcs.indexOf(amazonSideWidget.$iframe.attr('src'));
+    const lastIndex = arrayOfAmazonIframeSrcs.length - 1;
+    let resultIndex = 0;
+
+    if (direction === 'prev') {
+      resultIndex = currentIndex === 0 ? lastIndex : currentIndex - 1;
+    } else {
+      resultIndex = currentIndex === lastIndex ? 0 : currentIndex + 1;
+    }
+
+    return resultIndex;
+  }
+
+  amazonSideWidget.$prevBtn.on('click', function(e) {
+    amazonSideWidget.$iframe.attr('src', arrayOfAmazonIframeSrcs[getAmznIframeSrc('prev')]);
+  });
+
+  amazonSideWidget.$nextBtn.on('click', function(e) {
+    amazonSideWidget.$iframe.attr('src', arrayOfAmazonIframeSrcs[getAmznIframeSrc('next')]);
+  });
+  
+  amazonSideWidget.$closeBtn.on('click', function(e) {
+    amazonSideWidget.$self.toggleClass('is-closed');
+    //Change close btn icon
+    amazonSideWidget.$closeBtn.find('.fa').toggleClass('fa-angle-double-left').toggleClass('fa-angle-double-right');
+  });
+
+
+  //Affiliate Program Notification
+  const apnText = 'This site may contain Advertising and Affiliate Program links. As an Amazon Associate I earn from qualifying purchases.';
+  $('.page-footer').before('<div class="apn">' + apnText + '</div>');
 });
